41507eed0d7f884337712f2ffb29e8a60924ddc4
Tanay Thapar <tanay.ggmu@gmail.com>
2026-07-28 14:44:05 +0530

    Update backend CORS and add frontend code

--- app/main.py
+++ app/main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 from app.config import PROJECT_NAME
 from app.config import VERSION
@@ -13,6 +14,20 @@ Base.metadata.create_all(bind=engine)
 app = FastAPI(
     title=PROJECT_NAME,
     version=VERSION,
+)
+
+# CORS Middleware for Hackathon Prototype (Frontend & Local dev)
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=[
+        "https://biomind-hackverse2.netlify.app",
+        "http://localhost:5173",
+        "http://localhost:3000",
+        "*"
+    ],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
 )
 
 app.include_router(router)
